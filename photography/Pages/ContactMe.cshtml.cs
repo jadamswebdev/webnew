4f58ff5ffb03dee8d33eed694e148d1e096e81a3
Jeffrey Adams <60942973+jadamswebdev@users.noreply.github.com>
2020-06-05 23:51:30 -0500

    new

---
+++ photography/Pages/ContactMe.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace photography.Pages
+{
+    public class ContactMeModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ photography/Pages/DevLife.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace photography.Pages
+{
+    public class DevLifeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
